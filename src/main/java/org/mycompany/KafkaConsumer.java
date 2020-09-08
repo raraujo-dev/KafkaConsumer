f7cddac49483c8dea0adce495a503165b36c31eb
Rafael Araújo <rodrigues552@gmail.com>
2020-09-08 15:12:33 -0300

    first commit

---
+++ src/main/java/org/mycompany/KafkaConsumer.java
@@ -0,0 +1,26 @@
+package org.mycompany;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class KafkaConsumer extends RouteBuilder{
+
+	@Override
+	public void configure() throws Exception {
+		// TODO Auto-generated method stub
+		
+		
+		  from("kafka:{{kafka.topic}}?brokers={{kafka.host}}:{{kafka.port}}")
+          .log("Message received from Kafka : ${body}")
+          .log("    on the topic ${headers[kafka.TOPIC]}")
+          .log("    on the partition ${headers[kafka.PARTITION]}")
+          .log("    with the offset ${headers[kafka.OFFSET]}")
+          .log("    with the key ${headers[kafka.KEY]}");
+		
+		
+	}
+	
+	
+}
